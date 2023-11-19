7ad35479870c564ced6c4c33104804b4a0c361bf
Aditya Subramanian <aditarun2008@gmail.com>
2023-11-18 22:32:01 -0500

    stage3: attempt 1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -6,7 +9,14 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                let _ = stream.write(b"+PONG\r\n").unwrap();
+                let mut req: Vec<u8> = Vec::new();
+                stream.read_to_end(&mut req).unwrap();
+                let string = String::from_utf8(req).unwrap();
+                string.split("\r\n").for_each(|s| {
+                    if s == "PING" {
+                        let _ = stream.write_all(b"+PONG\r\n").unwrap();
+                    }
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
