da1de255f5e8b1e8faeff9e61037677c49890bbc
Aditya Subramanian <aditarun2008@gmail.com>
2023-11-19 14:11:31 -0500

    stage4: handle multiple connections

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,37 @@
-use std::{
-    io::{Read, Write},
-    net::TcpListener,
-};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            handle_stream(stream).await;
+        });
+    }
+}
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => loop {
-                let mut req = [0; 512];
-                stream.read(&mut req).unwrap();
-                let string = String::from_utf8_lossy(&req).to_string();
-                println!("request: {}", string);
-                string.split("\r\n").for_each(|s| {
-                    if s.to_lowercase() == "ping" {
-                        let _ = stream.write_all(b"+PONG\r\n").unwrap();
-                    }
-                });
-            },
+async fn handle_stream(stream: TcpStream) {
+    loop {
+        stream.readable().await.unwrap();
+        let mut buf = [0; 512];
+        match stream.try_read(&mut buf) {
+            Ok(n) => {
+                if n == 0 {
+                    break;
+                }
+            }
             Err(e) => {
-                println!("error: {}", e);
+                println!("failed to read from socket; err = {:?}", e);
+                continue;
+            }
+        }
+        let s = std::str::from_utf8(&buf).unwrap();
+        let x = s.split("\r\n");
+        for i in x {
+            if i.to_lowercase() == "ping" {
+                stream.writable().await.unwrap();
+                stream.try_write("+PONG\r\n".as_bytes()).unwrap();
             }
         }
     }
