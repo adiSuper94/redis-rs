73c6dca918bc771014d93af45fb3b84c83388f58
Aditya Subramanian <aditarun2008@gmail.com>
2024-11-22 17:41:48 -0500

    CLI tests (cli-test-1732315308881)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 pub mod redis_commands;
 pub mod redis_db;
 pub mod redis_server;
+use std::sync::Arc;
+
 use redis_commands::Command;
 use redis_server::{Redis, RedisCliArgs, Role};
-use tokio::net::{TcpListener, TcpStream};
+use tokio::{net::{TcpListener, TcpStream}, sync::Mutex};
 
 #[tokio::main]
 async fn main() {
@@ -48,7 +50,7 @@ fn parse_cli_args() -> RedisCliArgs {
         port,
         master_host: None,
         master_port: None,
-        role: Role::Primary
+        role: Role::Primary,
     };
     if let Some(replica_of) = replica_of {
         let replica_of: Vec<&str> = replica_of.split(" ").collect();
@@ -63,12 +65,13 @@ fn parse_cli_args() -> RedisCliArgs {
 }
 
 async fn handle_stream(stream: TcpStream, mut redis_server: Redis) {
+    let stream = Arc::new(Mutex::new(stream));
     loop {
-        if let Err(_) = stream.readable().await {
+        if let Err(_) = stream.lock().await.readable().await {
             continue;
         }
         let mut buf = [0; 512];
-        match stream.try_read(&mut buf) {
+        match stream.lock().await.try_read(&mut buf) {
             Ok(n) => {
                 if n == 0 {
                     break;
@@ -81,22 +84,7 @@ async fn handle_stream(stream: TcpStream, mut redis_server: Redis) {
         let req = String::from_utf8_lossy(&buf).to_string();
         let commands = Command::deserialize(&req);
         for command in commands {
-            let resp = redis_server.execute(&command).await;
-            let resp_bytes = resp.as_bytes();
-            write(&stream, resp_bytes).await;
-        }
-    }
-}
-
-async fn write(stream: &TcpStream, bytes: &[u8]) {
-    let mut offset = 0;
-    loop {
-        stream.writable().await.unwrap();
-        if let Ok(n) = stream.try_write(&bytes) {
-            offset += n;
-            if offset >= bytes.len() {
-                break;
-            }
+            redis_server.execute(command, Arc::clone(&stream)).await;
         }
     }
 }
